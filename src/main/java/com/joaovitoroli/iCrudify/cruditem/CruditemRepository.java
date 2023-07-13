3f16b94403ee9594c7456272cdc631c14b09d40c
João Vitor <joao.vitor012020@gmail.com>
2023-07-13 03:50:17 -0300

    feat: cria repository do model cruditem

---
+++ src/main/java/com/joaovitoroli/iCrudify/cruditem/CruditemRepository.java
@@ -0,0 +1,6 @@
+package com.joaovitoroli.iCrudify.cruditem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CruditemRepository extends JpaRepository<Cruditem, Long> {
+}
