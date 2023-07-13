a1d6e44d0944947347ce90e5463821b32508ff32
João Vitor <joao.vitor012020@gmail.com>
2023-07-13 01:06:36 -0300

    :tada:

---
+++ src/main/java/com/joaovitoroli/iCrudify/ICrudifyApplication.java
@@ -0,0 +1,13 @@
+package com.joaovitoroli.iCrudify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICrudifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICrudifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovitoroli/iCrudify/ICrudifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovitoroli.iCrudify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICrudifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
