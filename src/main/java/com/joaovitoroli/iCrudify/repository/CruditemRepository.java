ea315ca614495b185616e522ffeb3a52be9dcc8e
João Vitor <joao.vitor012020@gmail.com>
2023-07-15 01:45:27 -0300

    refactor: muda o tipo do id

--- src/main/java/com/joaovitoroli/iCrudify/repository/CruditemRepository.java
+++ src/main/java/com/joaovitoroli/iCrudify/repository/CruditemRepository.java
@@ -3,5 +3,5 @@ package com.joaovitoroli.iCrudify.repository;
 import com.joaovitoroli.iCrudify.model.Cruditem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CruditemRepository extends JpaRepository<Cruditem, Long> {
+public interface CruditemRepository extends JpaRepository<Cruditem, String> {
 }
