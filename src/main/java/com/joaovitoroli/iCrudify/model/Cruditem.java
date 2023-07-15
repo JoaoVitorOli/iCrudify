8d2fc3102dcf0b387d29cb8d6c6779881b865024
João Vitor <joao.vitor012020@gmail.com>
2023-07-15 01:44:03 -0300

    feat: cria constructor; muda tipo do id; gera setter

--- src/main/java/com/joaovitoroli/iCrudify/model/Cruditem.java
+++ src/main/java/com/joaovitoroli/iCrudify/model/Cruditem.java
@@ -1,12 +1,24 @@
 package com.joaovitoroli.iCrudify.model;
 
+import com.joaovitoroli.iCrudify.DTO.CruditemRequestDTO;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Table(name = "cruditems")
 @Entity(name = "cruditems")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Cruditem {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     private String title;
     private String image;
+
+    public Cruditem(CruditemRequestDTO data) {
+        this.image = data.image();
+        this.title = data.title();
+    }
 }
